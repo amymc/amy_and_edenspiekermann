a24c72c21c3f50c7c2564456db43f1691e6212e4
amymc <amy.mccarthy2@mail.dcu.ie>
2016-10-13 23:48:54 +0100

    rename helper to formatTime

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,6 @@
 requirejs.config({
   "baseUrl": "./src/",
   "paths": {
-    //"handlebars": ".././bower_components/require-handlebars-plugin/hbs/handlebars.runtime",
-   // "handlebars": '.././bower_components/handlebars/handlebars.min',
     "hbs": '.././bower_components/require-handlebars-plugin/hbs',
     "jquery": '.././bower_components/jquery/dist/jquery.min',
     "moment": '.././bower_components/momentjs/min/moment.min'
