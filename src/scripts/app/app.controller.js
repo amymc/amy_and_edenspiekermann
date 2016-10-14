c00618db28dfb6d726e30674e823fe1644d14549
amymc <amy.mccarthy2@mail.dcu.ie>
2016-10-14 02:16:06 +0100

    filter on author

--- src/scripts/app/app.controller.js
+++ src/scripts/app/app.controller.js
@@ -19,16 +19,26 @@ function AppController($, HandleBars, AppModel, imageItem) {
   Ctrl.prototype.addListeners = function addListeners() {
     console.log('add addListeners');
     //click on user, filter by user
-    this.$container.find('.js-author').on('click', this.filter);
+    this.$container.find('.js-author').on('click', this.filter.bind(this));
     // lazy load??
     //sort by date taken?
     //read more widget?
     
   };
 
-  Ctrl.prototype.filter = function filter() {
-    console.log('filter', this, $(this).attr("data-authorid"));
+  Ctrl.prototype.filter = function filter(e) {
+    e.preventDefault();
+    // console.log('filter', this, $(this).attr("data-authorid"));
+     var authorId = $(e.currentTarget).attr("data-authorid");
 
+    console.log('data', this.data, 'id', authorId);
+    var filteredData = this.data.filter(function (item) {
+      return item.author_id === authorId;
+    });
+
+
+    console.log(filteredData);
+    this.renderData(filteredData);
     //click on user, filter by user
     
   };
@@ -36,14 +46,14 @@ function AppController($, HandleBars, AppModel, imageItem) {
   Ctrl.prototype.getData = function getata() {
     this.model.get()
       .done(function cb(data) {
-         this.data = data;
-         this.renderData();
+         this.data = data.items;
+         this.renderData(this.data);
       }.bind(this));
   };
 
-  Ctrl.prototype.renderData = function renderData() {
-    console.log('renderData', this.data.items);
-    this.$container.html(imageItem({data: this.data.items}));
+  Ctrl.prototype.renderData = function renderData(data) {
+    console.log('renderData', this.data);
+    this.$container.html(imageItem({data: data}));
     this.addListeners(); 
   };
 
