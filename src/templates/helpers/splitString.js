1107929230cf260290c3b6d53c54309ef94b7d30
amymc <amy.mccarthy2@mail.dcu.ie>
2016-10-14 17:35:51 +0100

    display tags as seperate elements

--- src/templates/helpers/splitString.js
+++ src/templates/helpers/splitString.js
@@ -6,10 +6,14 @@ define([
 function (Handlebars) {
   function splitString (string, type) {
     var newStrings = string.split(/[() ]/);
+    var newStringsHtml = [];
     if (type === 'author') {
       return newStrings[1]
     }
-    return newStrings.join(' ');
+    for (var i = 0; i < newStrings.length; i++) {
+       newStringsHtml.push('<span>' + newStrings[i] + '</span>');
+    }
+    return newStringsHtml.join(' ');
   }
 
   Handlebars.registerHelper('splitString', splitString);
